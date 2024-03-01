5b93710196f23f1bb4eb15247480530c6c5b7972
Ccode1 <1131792721@qq.com>
2024-03-02 00:53:18 +0800

    [fix]上传server

---
+++ rpc-grpc-server/src/main/java/com/yhs/server/GrpcServer.java
@@ -0,0 +1,20 @@
+package com.yhs.server;
+
+import com.yhs.service.HelloServiceImpl;
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GrpcServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerBuilder serverBuilder = ServerBuilder.forPort(8082);
+        serverBuilder.addService((BindableService) new HelloServiceImpl());
+        Server server = serverBuilder.build();
+        server.start();
+        server.awaitTermination();
+
+    }
+}
